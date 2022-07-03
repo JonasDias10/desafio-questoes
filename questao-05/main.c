897bcaf9217da5dd0adab6d5391b69866a7344c4
Jonas Dias <zdias1413@gmail.com>
2022-07-03 11:54:36 -0300

    Adicionando a questão 5

---
+++ questao-05/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+int inverte_string(char *string);
+
+int main() {
+
+    char string[MAX];
+    printf("Entre com a string a ser invertida: ");
+    scanf(" %[^\n]", string);
+
+    inverte_string(string);
+
+    printf("\nString invertida: %s\n", string);
+
+    return 0;
+}
+
+int inverte_string(char *string){
+    int tamanho = (int) strlen(string);
+    if(tamanho <= 1)
+        return 0;
+    int meio = (int) tamanho/2;
+    for(int i = 0, j = tamanho - 1; i < meio; ++i, --j){
+        char auxiliar = string[j];
+        string[j] = string[i];
+        string[i] = auxiliar;
+    }
+    return 1;
+}
